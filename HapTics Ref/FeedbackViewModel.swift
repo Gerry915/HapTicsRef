0015827426a384ba9efb37cb468da78b78702d3b
Gerry915 <gao88915@gmail.com>
2022-02-01 21:46:02 +1100

    init remote

---
+++ HapTics Ref/FeedbackViewModel.swift
@@ -0,0 +1,16 @@
+//
+//  FeedbackViewModel.swift
+//  HapTics Ref
+//
+//  Created by Gerry Gao on 29/1/2022.
+//
+
+import UIKit
+
+class FeedbackViewModel {
+    
+    var feedbackList: [Feedback]?
+    
+    
+    
+}
